c28175c597eb1cbc6461758f3af23e9e519ea0b1
Omar Belaizi <80468732+flytoox@users.noreply.github.com>
2023-08-28 00:23:38 +0100

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -87,7 +87,7 @@ bool isPrime(ll n)
  
 bool isPalin(string s)
 {
-    for (int i = 0; i < s.length() / 2; i++)
+    for (int i = 0; i < (int)s.length() / 2; i++)
         if (s[i] != s[s.length() - i - 1])
             return false;
     return true;
